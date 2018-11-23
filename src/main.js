ebe561399340ba4e370098a3291b36ad5ddce497
山本大 <dai.yamamoto@levelenter.com>
2018-11-23 23:27:16 +0900

    bugfix

--- src/main.js
+++ src/main.js
@@ -1,20 +1,25 @@
+
 AFRAME.registerComponent('ahead',{
 	schema: {
 		type:'number', default : '1'
 	},
 	tick: function (time, timeDelta) {
-		// console.log(this,this.attrValue,time,timeDelta);
+
 		let speed = (this.attrValue ? this.attrValue : 1) / 60;
 
-		this.position.x += -Math.cos((this.rotation.y - 90) * Math.PI / 180) * speed;
-		this.position.z += Math.sin((this.rotation.y - 90) * Math.PI / 180) * speed;
+		let position = this.camera.getAttribute('position');
+		let rotation = this.camera.getAttribute('rotation');
+
+		position.x += -Math.cos((rotation.y - 90) * Math.PI / 180) * speed;
+		position.z += Math.sin((rotation.y - 90) * Math.PI / 180) * speed;
 		this.camera.setAttribute('position', this.position);
 	},
 
 	init: function () {
 		this.camera = document.querySelector('a-camera');
-		this.position = this.camera.getAttribute('position');
-		this.rotation = this.camera.getAttribute('rotation');
+		if(!this.camera){
+			this.camera = document.querySelector('[camera]')
+		}
 	}
 });
 
